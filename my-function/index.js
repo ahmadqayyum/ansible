067b6ea32d75485f5f600815911fadd0a47a36cd
jamilaQayyum <jamilaqayumch@gmail.com>
2020-03-06 17:31:40 +0000

    iintial commit

--- my-function/index.js
+++ my-function/index.js
@@ -1,7 +1,7 @@
 exports.handler = async (event) => {
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from Ahmad Lambda Github Actions!'),
+        body: JSON.stringify('Change for Nidhi Github Actions!'),
     };
     return response;
 };

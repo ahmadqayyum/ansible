346b16f34417b6b6642383624ccbceec2253e89c
Ahmad Qayyum <52801818+ahmadqayyum@users.noreply.github.com>
2020-03-11 23:46:49 +0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+exports.handler = async (event) => {
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify('Change for Nidhi Github Actions!'),
+    };
+    return response;
+};

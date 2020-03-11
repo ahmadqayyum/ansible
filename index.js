b4aedd3711f6aef36ae951f91c467adbabc73528
Ahmad Qayyum <52801818+ahmadqayyum@users.noreply.github.com>
2020-03-12 00:02:23 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 exports.handler = async (event) => {
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Change for Nidhi Github Actions!'),
+        body: JSON.stringify('changes from git to azure then lambda'),
     };
     return response;
 };

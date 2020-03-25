b78dc1f552be0c561ceea59a3b09b9f011d6f61f
Ahmad <m.ahmad@zigron.com>
2020-03-25 16:01:38 +0500

    unit test

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-exports.handler = async (event) => {
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('changes from git to azure then lambda'),
-    };
-    return response;
-};
+//Load express module with `require` directive
+var express = require('express')
+var app = express()
+
+//Define request response in root URL (/)
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+//Launch listening server on port 8080
+app.listen(8080, function () {
+  console.log('App listening on port 8080!')
+})

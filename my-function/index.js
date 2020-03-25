41acf84ed33a21bc514548ba06f620775043d2db
Ahmad <m.ahmad@zigron.com>
2020-03-25 15:06:27 +0500

    some unnecessary files deleted

--- my-function/index.js
+++
@@ -1,7 +0,0 @@
-exports.handler = async (event) => {
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('Change for Nidhi Github Actions!'),
-    };
-    return response;
-};
